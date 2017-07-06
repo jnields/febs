885fe412fbe21f2c5ac110b304041c33092e5cdf
Alex Perkins <aperkin@rei.com>
2017-07-06 08:38:03 -0700

    Code cleanup.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const wp = require('webpack');
 const logger = require('./lib/logger');
 const util = require('./lib/util');
 const merge = require('webpack-merge');
-const path = require('path');
+
+// Environmental webpack.conf (dev or prod)
+const getBaseConf = () => require(`./webpack.${process.env.NODE_ENV}.conf`);
 
 /**
  * Create's compiler instance with appropriate environmental
@@ -16,15 +18,10 @@ const path = require('path');
  * @return {Object} Webpack compiler instance.
  *
  */
-
-// Environmental webpack.conf (dev or prod)
-const getBaseConf = () => require(`./webpack.${process.env.NODE_ENV}.conf`);
-
 const createCompiler = (conf) => {
   const confBase = getBaseConf();
 
   // Overrides config. Override the environmental config defaults.
-  // const confOverridesFile = './webpack.overrides.conf.js';
   let confOverrides;
 
   if (conf.confPath) {
@@ -33,16 +30,12 @@ const createCompiler = (conf) => {
     confOverrides = conf; // used for unit testing only.
   }
 
-
   // Always replace:
-  //  entry
-  //  output
+  //   - entry, output
   const wpConf = merge.strategy({
     entry: 'replace',
     output: 'replace',
   })(confBase, confOverrides);
-
-  // logger.info('Webpack conf:', wpConf);
 
   // Create webpack compiler object with merged config objects.
   return wp(wpConf);
@@ -67,38 +60,8 @@ const compile = conf => createCompiler(conf).run((err, stats) => {
   }));
 });
 
-/**
- * Start webpack dev server.
- *
- * Merge overrides and dev confs.
- * Fire it up.
- */
-const devServer = (conf) => {
-  const WDS = require('webpack-dev-server');
-  const compiler = createCompiler(conf);
-
-  // Go.
-  const port = 9000;
-  const server = new WDS(compiler, {
-    port,
-    stats: {
-      colors: true,
-      detailed: true,
-    },
-    contentBase: path.join(__dirname, '../dest'),
-    publicPath: '/dest/',
-    compress: true,
-    clientLogLevel: 'info',
-    open: true,
-  });
-  server.listen(9000, '127.0.0.1', () => {
-    console.log(`Starting server on http://localhost:${port}`);
-  });
-};
-
 // febs api
 module.exports = {
-  devServer,
   compile,
   createCompiler,
 };
