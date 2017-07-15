c3d80cf52afa5f0f360307c7119a9db2a349c2cd
Alex Perkins <aperkin@rei.com>
2017-07-14 19:21:49 -0700

    Clean up dev server code.

--- src/dev-server.js
+++ src/dev-server.js
@@ -1,10 +1,18 @@
+/**
+ * webpack-dev-server module.
+ *
+ * Starts the dev-server at localhost:8080.
+ *
+ */
+
 const WDS = require('webpack-dev-server');
-const path = require('path');
 
 /**
- * Start webpack dev server.
+ * Run the webpack-dev-server.
+ *
+ * @param {Object} compiler Webpack instance configured with a webpack.conf.js
  */
-module.exports = (compiler) => {
+const runDevServer = (compiler) => {
   const port = 8080;
   const cwd = process.cwd();
   const server = new WDS(compiler, {
@@ -13,16 +21,20 @@ module.exports = (compiler) => {
       colors: true,
       detailed: true,
     },
-    // contentBase: path.join(__dirname, '../dest'),
-    // contentBase: path.join(cwd, 'dest'),
     contentBase: cwd,
     publicPath: '/dest/',
     compress: true,
     clientLogLevel: 'info',
+
+    // These options *should* open a new browser but apparently aren't supported
+    // in the node api.
     open: true,
     openPage: '',
   });
   server.listen(port, '127.0.0.1', () => {
     console.log(`Starting server on http://localhost:${port}`);
   });
+  return server;
 };
+
+module.exports = runDevServer;
