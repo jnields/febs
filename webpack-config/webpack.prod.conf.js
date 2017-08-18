a4c7f18a55957cdf717d2940c58eb26f603a99d6
Alex Perkins <aperkin@rei.com>
2017-08-18 12:06:46 -0700

    Add output hash entry to prod bundle.

--- webpack-config/webpack.prod.conf.js
+++ webpack-config/webpack.prod.conf.js
@@ -2,8 +2,16 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const cssProcessor = require('cssnano');
+const path = require('path');
+
+const projectPath = process.cwd();
 
 module.exports = {
+
+  output: {
+    path: path.resolve(projectPath, 'dist'),
+    filename: '[name].bundle-[hash].js',
+  },
 
   devtool: 'source-map', // external
 
