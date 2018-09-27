f670ac4b8b462c5387e7597062abcbdd0be1d7fd
Alex Perkins <aperkin@rei.com>
2018-09-27 06:41:37 -0700

    Remove the global entry style, scope to the Vue component.

--- templates/src/js/components/App.vue
+++ templates/src/js/components/App.vue
@@ -5,22 +5,24 @@
 </template>
 
 <script>
-export default {
-  name: 'App',
-  props: {
-    message: {
-      type: String,
-      default: 'default message',
+  export default {
+    name: 'App',
+    props: {
+      message: {
+        type: String,
+        default: 'default message',
+      },
     },
-  },
-};
+  };
 </script>
 
 <style scoped>
-    #c1 {
-        background-color: #39AF78;
-        color: #000000;
-        padding: 20px;
-        text-align: center;
-    }
+  #c1 {
+    background-color: #39AF78;
+    color: #000;
+    padding: 20px;
+    text-align: center;
+    border: solid;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
 </style>
